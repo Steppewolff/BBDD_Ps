bd7f1514d285f73f03767f621f088fc9592a8d07
Steppewolf <gomez.romano@gmail.com>
2024-01-26 07:35:02 +0100

    modificación en excel_to_sql.py para convertirlo en app Tkinter, pequeños ajustes en db.py

---
+++ db/db.py
@@ -0,0 +1,169 @@
+# Importación de paquetes externos al proyecto
+# import sys
+# sys.setdefaultencoding('utf-8')
+import pymysql.cursors
+from werkzeug.security import generate_password_hash
+import sqlalchemy as db
+import logging
+from datetime import datetime
+
+# Bloque para configurar el logger de Python, en el directorio Top: Python_debug.log
+logger = logging.getLogger('Python_Log')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('Python_debug.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+
+# logger.debug('Debug Message')
+# logger.info('Info Message')
+# logger.warning('Warning')
+# logger.error('Error Occurred')
+# logger.critical('Critical Error')
+
+class PsDb:
+    # Conexion a la BBDD del servidor mySQL
+    def connect(self):
+        self.db = pymysql.connect(
+            host='127.0.0.1',  # 'fgromano.com', #
+            port=3306,
+            user='fernando',  # 'root',
+            passwd='password',
+            db='psdb',
+            charset='utf8mb4',
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor)
+        self.cursor = self.db.cursor()
+
+    # Desconexion de la BBDD del servidor mySQL
+    def disconnect(self):
+        self.db.close()
+
+    # Autenticación del usuario usando JWT-flask
+    def jwtAuth(self, username):
+        sql = "SELECT * FROM arpbigidisba_users WHERE username = '" + username + "';"
+        self.cursor.execute(sql)
+        Response = self.cursor.fetchone()
+        return Response
+
+    # Obtención de datos del usuario a partir de su ID usando JWT-flask
+    def jwtId(self, user_id):
+        sql = "SELECT * FROM arpbigidisba_users WHERE user_id = '" + str(user_id) + "';"
+        self.cursor.execute(sql)
+        Response = self.cursor.fetchone()
+        return Response
+
+    # Devuelve los nombres de las columnas de una tabla
+    def get_variable_names(self, table):
+        sql = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS " \
+              "WHERE " \
+              "TABLE_SCHEMA = 'psdb' " \
+              "AND " \
+              "TABLE_NAME = '" + table + "';"
+
+        self.cursor.execute(sql)
+        Response = self.cursor.fetchall()
+
+        return Response
+
+    # Devuelve los datos de tabla summary en psdb
+    def summary_get(self, pag):
+        already_displayed = 5 * pag
+        sql = "SELECT * FROM psdb.summary" \
+              "ORDER BY summmary_id " \
+              "LIMIT " + str(already_displayed) + ", 5;"
+
+        self.cursor.execute(sql)
+        Response = self.cursor.fetchall()
+
+        # for isolate in Response:
+        #     isolate["isolate_info"] = isolate['isolate_id'] + "/"
+        #     isolate['strain_id']            
+
+        return Response
+
+    # Devuelve los datos de un aislado concreto mediante isolated_id
+    def isolated_id(self, isolated_id):
+        sql = "SELECT * FROM psdb.summary" \
+              "ORDER BY summmary_id " \
+              "WHERE isolated_id = '" + isolated_id + "';"
+
+        print('SQL query: ', sql)
+        self.cursor.execute(sql)
+        Response = self.cursor.fetchall()
+
+        return Response
+
+        # Devuelve los datos de películas
+    # def movies_post(self, pag, campos):
+    #     already_displayed = 5*pag
+
+    #     logger.info("NOTA: ", campos)
+
+    #     sql = "SELECT title_akas.id AS id,"\
+    #             "title_akas.title AS titulo,"\
+    #             "title_basics.genres AS genero,"\
+    #             "title_basics.endYear AS anho,"\
+    #             "title_ratings.averageRating AS nota,"\
+    #             "title_basics.runtimeMinutes AS duracion, "\
+    #             "title_akas.titleId AS titleId "\
+    #             "FROM title_akas "\
+    #             "JOIN title_basics "\
+    #             "ON title_akas.titleId = title_basics.tconst "\
+    #             "JOIN title_ratings "\
+    #             "ON title_ratings.tconst = title_basics.tconst"
+
+    #     # if campos para WHERE
+    #     if len(campos[0]) > 0: 
+    #         sql=sql+" HAVING"
+    #         filter = campos[0]
+
+    #         words = 0
+    #         for a in filter:
+    #             # if position > 0:
+    #             #     sql=sql+" AND "
+    #             if (a == 'genero' and len(filter[a])>0):
+    #                 # words = 0
+    #                 for word in filter[a].split(','):
+    #                     if words > 0:
+    #                         sql=sql+" AND "
+    #                     sql=sql+" "+ a + " LIKE '%"+ word +"%'"
+    #                     words =+ 1
+    #             if (a == 'nota'):
+    #                 if (words > 0):
+    #                     sql=sql+" AND "
+
+    #                     logger.info("NOTA 2: ", word)
+
+    #                 sql=sql+" "+ a + " " + filter[a][0]+ filter[a][-3:-1]
+
+    #             position =+ 1
+
+    #      # if campos para ORDER BY
+    #         if len(campos[1]) > 0: 
+    #             sql=sql+" ORDER BY"
+    #             order = campos[1]
+
+    #             for a in order:
+    #                 if a in ['anho', 'nota', 'titulo']:
+    #                     sql=sql+" "+ a +" "+ order[a]
+    #             sql=sql+", id"                
+
+    #         else:
+    #             sql=sql+" ORDER BY id"
+
+    #     sql=sql+" LIMIT " + str(already_displayed) + ", 5;"
+
+    #     logger.debug('SQL: ', sql)
+
+    #     self.cursor.execute(sql)
+    #     Response = self.cursor.fetchall()
+
+    #     for movie in Response:
+    #         movie["url"] = "https://www.imdb.com/title/" + movie['titleId'] + "/"
+    #         movie.pop('id')
+
+    #     return Response
--- excel_to_sql.py
+++ excel_to_sql.py
@@ -1,13 +1,41 @@
 import pandas as pd
 import numpy as np
-import tkinter.filedialog
+import tkinter as tk
+from tkinter import filedialog, ttk
 import db
 
-file_path = tkinter.filedialog.askopenfilename()
+# file_path = tkinter.filedialog.askopenfilename()
+# Instantiation of a tk app
+app = tk.Tk()
 
-df = pd.read_excel(file_path)
+# Title and parameters of the app
+app.title('CARGA DE DATOS A BBDD ARPBIGIDISBA')
+app.geometry('800x600')
 
-db_obj = db.PsDb()
+# Create Text field for Excel column names
+excelColumnNames_text = tk.Text(app, height=20, width=50)
+excelColumnNames_text.grid(column=2, row=2)
+
+# Create Text field for DB fields
+dbFieldNames_text = tk.Text(app, height=20, width=50)
+dbFieldNames_text.grid(column=60, row=2)
+
+
+def open_excel_file():
+    filetypes = (('xls files', '*.xls'),
+                 ('xlsx files', '*.xlsx'),
+                 ('csv files', '*.csv'))
+
+    file_path = filedialog.askopenfile(filetypes=filetypes, initialdir="/home/fernando/Proyectos")
+    df = pd.read_csv(file_path, encoding='utf-8')
+
+    return df
+
+
+# Button to open file
+chooseFileButton = ttk.Button(app, text='Choose File', command=open_excel_file)
+
+db_obj = db.db.PsDb()
 db_obj.connect()
 table_name = 'ab_resistance'
 result = db_obj.get_variable_names(table_name)
@@ -15,37 +43,52 @@ db_obj.disconnect()
 
 variable_list = [dic['COLUMN_NAME'] for dic in result]
 
+# df = open_excel_file()
+
 for excel_column in df.columns:
     match = 0
     match_variable = ""
+    excelColumn_text = excel_column + "_text"
+
 
     # substrings is a list with words in excel column name
     substrings = excel_column.split(" ")
 
-#    if len(substrings) > 1:
-#        primera_subcadena = substrings[0]
-#        segunda_subcadena = substrings[1]
+    #    if len(substrings) > 1:
+    #        primera_subcadena = substrings[0]
+    #        segunda_subcadena = substrings[1]
 
-#        print(f'Primera subcadena: {primera_subcadena}')
-#        print(f'Segunda subcadena: {segunda_subcadena}')
-#    else:
-#        print('La cadena no contiene un espacio o tiene más de un espacio.')
+    #        print(f'Primera subcadena: {primera_subcadena}')
+    #        print(f'Segunda subcadena: {segunda_subcadena}')
+    #    else:
+    #        print('La cadena no contiene un espacio o tiene más de un espacio.')
 
     for variable in variable_list:
         for substring in substrings:
-#            print("substring: " + substring.lower() + ", " + "variable: " + variable)
+            #            print("substring: " + substring.lower() + ", " + "variable: " + variable)
             if substring.lower() in variable.lower():
                 match = 1
                 match_variable = variable
-#                print(match_variable)
+    #                print(match_variable)
 
     if match == 1:
         print(excel_column.lower() + " --> " + match_variable)
     else:
         print(excel_column.lower() + "---")
 
-# Imprimir la lista
-print(variable_list)
+    excelColumnNames_text.insert('1.0', excel_column)
+    # dbFieldNames_text.insert('1.0', )
 
-#print(df[['ST']].to_string(index=False))
-#print(result)
+# print(df[['ST']].to_string(index=False))
+# print(result)
+# Imprimir la lista
+
+
+print(variable_list)
+print(df.columns)
+
+# Specify the button position on the app
+chooseFileButton.grid(sticky='w', padx=250, pady=50)
+
+# Make infinite loop for displaying app on the screen
+app.mainloop()
