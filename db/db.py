f0cecf39c2b827c23e824064572b723fa54dddd5
Steppewolf <gomez.romano@gmail.com>
2024-01-28 01:00:06 +0100

    modificaciones en excel_to_sql_v5

--- db/db.py
+++ db/db.py
@@ -31,7 +31,7 @@ class PsDb:
             host='127.0.0.1',  # 'fgromano.com', #
             port=3306,
             user='fernando',  # 'root',
-            passwd='password',
+            passwd='Orgullovalor',
             db='psdb',
             charset='utf8mb4',
             autocommit=True,
--- excel_to_sql_v5.py
+++ excel_to_sql_v5.py
@@ -1,19 +1,21 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 import pandas as pd
 import db
+
 
 class ExcelToSqlConverter:
     def __init__(self, root):
         self.root = root
         self.root.title("Excel to SQL Converter")
-        self.root.geometry('800x700')
+        self.root.geometry('800x600')
 
         # Variables para almacenar nombres de campos y opciones de select
         self.field_names = []
+        self.select_fields = []
         self.select_options = []
 
-        # Interfaz gráfica
+        # Importar datos
         self.read_input_file()
 
     def read_input_file(self):
@@ -31,15 +33,29 @@ class ExcelToSqlConverter:
         if file_path:
             try:
                 df = pd.read_excel(file_path) if file_path.endswith(('.xls', '.xlsx')) else pd.read_csv(file_path)
-                self.field_names = df.columns.tolist()
-                self.excel_len = len(self.field_names)
+                # self.field_names = df.columns.tolist()
+                self.excel_fields = df.columns.tolist()
+
+                print('excel_fields:')
+                print(self.excel_fields)
 
                 # Mostrar nombres de campos en campos de texto y opciones de select
-                for i, field in enumerate(self.field_names):
-                    self.excel_fields[i].delete(0, tk.END)
-                    self.excel_fields[i].insert(0, field)
+                # for i, field in enumerate(self.field_names):
+                #     print('i, field:')
+                #     print(i)
+                #     print(field)
+                #     #self.excel_fields[i].delete(0, tk.END)
+                #     self.excel_fields[i].insert(0, field)
+
+                # for i, field in enumerate(self.excel_fields):
+                #     print('i, field:')
+                #     print(i)
+                #     print(field)
+                #     #self.excel_fields[i].delete(0, tk.END)
+                #     self.excel_fields[i].insert(0, field)
 
                 # Conectar a la base de datos MySQL para obtener nombres de campos de la tabla
+
                 db_obj = db.db.PsDb()
                 db_obj.connect()
                 table_name = 'ab_resistance'
@@ -47,30 +63,22 @@ class ExcelToSqlConverter:
                 db_obj.disconnect()
 
                 self.select_options = [dic['COLUMN_NAME'] for dic in result]
+
+                print('select_options:')
                 print(self.select_options)
-#                    variable_list = [dic['COLUMN_NAME'] for dic in result]
-
-                # Mostrar opciones en el campo select
-                self.select_field.delete(0, tk.END)
-                for option in self.select_options:
-                    self.select_field.insert(tk.END, option)
-
-                if db_obj.db.open:
-                    db_obj.disconnect()
 
             except pd.errors.EmptyDataError:
                 print("Error: El archivo está vacío.")
             except Exception as e:
                 print(f"Error al leer el archivo: {e}")
 
-        # Interfaz gráfica
-        self.create_interface()
+            # Interfaz gráfica
+            self.create_interface()
 
     def create_interface(self):
         # Crear un canvas para contener la barra de desplazamiento y la interfaz gráfica
         self.canvas = tk.Canvas(self.root)
         self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        print(self.excel_len)
 
         # Barra de desplazamiento vertical
         scrollbar = tk.Scrollbar(self.root, orient=tk.VERTICAL, command=self.canvas.yview)
@@ -90,38 +98,39 @@ class ExcelToSqlConverter:
     def create_interface_elements(self):
         # Crear campos de texto en la columna izquierda
         tk.Label(self.frame, text="Campos de Archivo de Entrada").grid(row=0, column=0, columnspan=2)
-        self.excel_fields = []
+        #        self.excel_fields = []
 
         # Crear campos select en la columna derecha
         tk.Label(self.frame, text="Campos de Tabla MySQL").grid(row=0, column=15)
         self.db_fields = []
         self.db_values = []
 
-        for i in range(self.excel_len):  # Número de campos en el excel
-            entry = tk.Entry(self.frame)
-            entry.grid(row=i+2, column=0, sticky="w", padx=5, pady=5)
+        for i in range(len(self.excel_fields)):  # Número arbitrario de campos en el excel
+            entry_text = tk.StringVar()
+            entry = tk.Entry(self.frame, textvariable=entry_text)
+            entry.grid(row=i + 2, column=0, sticky="w", padx=5, pady=5)
+            entry_text.set(self.excel_fields[i])
             self.excel_fields.append(entry)
-
-            # Variable to store the options list of the OptionMenu, they are the DB fields
-            options_list = ["Option1", "Option2", "Option3", "Option4"]
 
             # Variable to store the option selected in OptionMenu
             db_values = tk.StringVar(root)
 
-            print(self.select_options)
             db_values.set("Select an Option")
-            self.select_field = tk.OptionMenu(self.frame, db_values, *self.select_options)
-            self.select_field.grid(row=i+2, column=15, sticky="w", padx=5, pady=5)
+            #            self.select_field = tk.OptionMenu(self.frame, db_values, *self.select_options)
+            self.select_field = ttk.Combobox(self.frame, state="normal", textvariable=db_values, values=self.select_options)
+            self.select_field.grid(row=i + 2, column=15, sticky="w", padx=5, pady=5)
 
         # Variable to store the option selected in OptionMenu
         # value_inside = tk.StringVar(root)
 
         # Botón para seleccionar archivo de entrada
-        tk.Button(self.frame, text="Leer archivo de entrada", command=self.read_input_file).grid(row=1, column=0, columnspan=2, pady=10)
+        tk.Button(self.frame, text="Leer archivo de entrada", command=self.read_input_file).grid(row=1, column=0,
+                                                                                                 columnspan=2, pady=10)
 
     def on_canvas_configure(self, event):
         # Configurar el tamaño del canvas cuando cambia el tamaño de la ventana
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
 
 if __name__ == "__main__":
     root = tk.Tk()
